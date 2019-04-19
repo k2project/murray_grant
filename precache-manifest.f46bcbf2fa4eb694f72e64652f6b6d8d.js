617c2017127fd5d2faf01d62f790f90574643ef6
k2project <kopczkrzy@gmail.com>
2019-04-19 09:13:25 +0100

    Updates

---
+++ precache-manifest.f46bcbf2fa4eb694f72e64652f6b6d8d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b332987a623a7726b9d6133b5bcdfdb",
+    "url": "/murray_grant/static/media/fts_white.7b332987.png"
+  },
+  {
+    "revision": "8f3c3408f68a641f5a3a",
+    "url": "/murray_grant/static/css/main.694a510a.chunk.css"
+  },
+  {
+    "revision": "8b2f7d9a194e17c6ff80",
+    "url": "/murray_grant/static/js/runtime~main.4d3a238e.js"
+  },
+  {
+    "revision": "0e5ed0f9d5d1aa937a2f",
+    "url": "/murray_grant/static/js/2.48bbcafb.chunk.js"
+  },
+  {
+    "revision": "75a2c5cf4b70bd67950161e0ebacd16c",
+    "url": "/murray_grant/static/media/grey.75a2c5cf.jpg"
+  },
+  {
+    "revision": "c70cb454dbd553028b59de66b0c25911",
+    "url": "/murray_grant/static/media/grey2.c70cb454.jpg"
+  },
+  {
+    "revision": "b2f51b365105539b75c0a28d8c79ebea",
+    "url": "/murray_grant/static/media/MGAfundation.b2f51b36.png"
+  },
+  {
+    "revision": "be14cc7290316e726ca1ff3a5caaa18c",
+    "url": "/murray_grant/static/media/bbe.be14cc72.png"
+  },
+  {
+    "revision": "9ced1bfdb9afec3e91b3a8481acb9b4f",
+    "url": "/murray_grant/static/media/bbp.9ced1bfd.png"
+  },
+  {
+    "revision": "3c11f9ac4c8acf8c829b8273ecf03ab8",
+    "url": "/murray_grant/static/media/ds_white.3c11f9ac.png"
+  },
+  {
+    "revision": "8f3c3408f68a641f5a3a",
+    "url": "/murray_grant/static/js/main.4d2b09d6.chunk.js"
+  },
+  {
+    "revision": "300e85fe03ea3338f838b698d4ee9b9a",
+    "url": "/murray_grant/static/media/mgaAcademy.300e85fe.png"
+  },
+  {
+    "revision": "f07b28c7ce54453447ffd92d99bfc57d",
+    "url": "/murray_grant/static/media/mgam_white.f07b28c7.png"
+  },
+  {
+    "revision": "f648a7c329261ad7bc4e7882104adc27",
+    "url": "/murray_grant/static/media/prudential.f648a7c3.png"
+  },
+  {
+    "revision": "f6a733a44788172f0ac960d7b28b6245",
+    "url": "/murray_grant/static/media/sykes.f6a733a4.png"
+  },
+  {
+    "revision": "10e2eb910100eedf34016535885ecce6",
+    "url": "/murray_grant/static/media/ted.10e2eb91.svg"
+  },
+  {
+    "revision": "78421432f5c49925aa33effc40a117b2",
+    "url": "/murray_grant/static/media/bsb.78421432.jpg"
+  },
+  {
+    "revision": "834e78ccb3e2a3d13f3b494ae84508ed",
+    "url": "/murray_grant/static/media/gsd.834e78cc.jpg"
+  },
+  {
+    "revision": "12e3c253dd6a20ae074b21f4d0dc369d",
+    "url": "/murray_grant/static/media/mg5.1.12e3c253.jpg"
+  },
+  {
+    "revision": "f9979fb99cf21cbede8031b19d070112",
+    "url": "/murray_grant/static/media/msn.f9979fb9.JPG"
+  },
+  {
+    "revision": "b615a10509c3237bce24226aefaf7a1a",
+    "url": "/murray_grant/index.html"
+  }
+];
