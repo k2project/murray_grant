f8cfb0f1de91256389444a1a53f5fe67d3176710
k2project <kopczkrzy@gmail.com>
2019-04-23 13:29:54 +0100

    Updates

--- precache-manifest.6ab317b6fc5a9abef17604430dc8969d.js
+++ precache-manifest.6ab317b6fc5a9abef17604430dc8969d.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/murray_grant/static/media/fts_white.7b332987.png"
   },
   {
-    "revision": "587ae1bd64dbbb9cd078",
-    "url": "/murray_grant/static/css/main.c8c3a7c6.chunk.css"
+    "revision": "5bfb829979fa68dbcaa1",
+    "url": "/murray_grant/static/css/main.18592890.chunk.css"
   },
   {
     "revision": "8b2f7d9a194e17c6ff80",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/murray_grant/static/media/ds_white.3c11f9ac.png"
   },
   {
-    "revision": "587ae1bd64dbbb9cd078",
+    "revision": "5bfb829979fa68dbcaa1",
     "url": "/murray_grant/static/js/main.f80b8233.chunk.js"
   },
   {
@@ -80,7 +80,7 @@ self.__precacheManifest = [
     "url": "/murray_grant/static/media/msn.f9979fb9.JPG"
   },
   {
-    "revision": "bb498cca403462c7e1e6ab7aace8709e",
+    "revision": "07d938a9041077ab3e2c51f31c08f154",
     "url": "/murray_grant/index.html"
   }
 ];
