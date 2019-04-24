ab7dff45d86585ef6854d5ba1db680583b6fe419
k2project <kopczkrzy@gmail.com>
2019-04-24 20:17:45 +0100

    press part2

--- src/pages/Press/Press.js
+++ src/pages/Press/Press.js
@@ -19,22 +19,31 @@ export default function Press (){
     )
 }
 function displayArticle(article){
-    const{cls, date, author, source, title, subtitle, text, img} = article;
+    const{cls, date, author, source, title, subtitle, text, img, link} = article;
     const bg = require('./../../media/imgs/'+img.path);
     return (
         <article className={cls}>
             <div className="article__img" style={{backgroundImage:"url("+bg+")"}}>
                 <div className="article__img_caption">{img.caption}</div>
+                <div className="cover"></div>
             </div>
-            <div className="article__body">
+            <div className="article__heading">
                 <div className="article__top">
                     <div className="article__title font_themed">{title}</div>
                     <p>by <b>{author}</b> — Posted in <b>{source}</b> on <b>{date}</b></p>
                 </div>
                 <p className="article__subtitle">{subtitle}</p>
                 <div className="btn" onClick={e=>displayArticleText(e)}> Read more</div>
+                <div className="article__arr">&#8595;</div>
             </div>
-            {text}
+            <div className="article__body">
+                {text}
+                <p className="article__link">
+                    <a href={link} target="_blank"  rel="noopener noreferrer">{source}'s original article available here.</a>
+                    <br/>
+                    <span onClick={e=>closeArticleText(e)}>&#10006; Close</span>
+                </p>
+            </div>
         </article>
     )
 
@@ -42,4 +51,10 @@ function displayArticle(article){
 function displayArticleText(e){
     e.target.style.display = 'none';
     e.target.closest('article').classList.add('open');
+}
+function closeArticleText(e){
+    const article = e.target.closest('article');
+    article.classList.remove('open');
+    article.querySelector('.btn').style.display = 'block';
+    article.scrollIntoView({block: "center"});
 }
