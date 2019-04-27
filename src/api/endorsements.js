0ff1d5b850683bcfe1fb613cf4094dc1a66475ca
k2project <kopczkrzy@gmail.com>
2019-04-27 09:57:20 +0100

    endoresemts

---
+++ src/api/endorsements.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const markLethem = {
+    name:'Mark Lethem',
+    title:'Managing Director at IAB',
+    link:'https://www.iabarcelona.com/',
+    text:'Murray has been associated with the IAB from its launch and was instrumental in its original recruitment campaigns. Over the years Murray has represented the IAB with a series of successful international masterclass tours. Murray´s classes and audition masterclasses are always very popular with students. He has been a loyal supporter of the IAB from the outset and has helped us with his talent and advice.'
+}
+const sarahMilne = {
+    name:'Sarah Milne',
+    title:'Principal of Cyprus Performing Arts',
+    link:'http://www.cyp-arts.com/',
+    text:'Murray has visited my Performing Arts school &quot;Cyprus Performing Arts&quot; in Nicosia on two occasions. Murray is a true professional with valuable industry knowledge. His choreography is amazing. He has a great repport with the students, he inspires and challenges everyone when he enters the building. I have already booked him to visit again next year and we all can\'t wait for his visit.'
+}
+const natalieWeiss = {
+    name:'Natalie Weiss',
+    title:'YouTube &amp; Broadway Singer',
+    link:'https://natalieweissofficial.com/',
+    text:'Murray produced/organized (you can change this wording) my entire European tour.  He is extremely efficient and detail-oriented during the booking process.  His budgeting and negotiating skills are unparalleled and you can always rely on him to respond in a timely manner.'
+}
+
+let endorsements = [
+    natalieWeiss,
+    sarahMilne,
+    markLethem,
+];
+export {endorsements};
--- src/pages/Bio/Endorsements/Endorsements.js
+++ src/pages/Bio/Endorsements/Endorsements.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import './Endorsements.scss';
+
+import {endorsements} from './../../../api/endorsements';
 import quotes from './../../../media/icons/quotes.png';
 
 export default function Endorsements (){
+    const endorsementsList = endorsements.map(endorsement => displayEndorsements(endorsement))
     return(
         <section>
+            <div className="section__title">Industry Endorsements</div>
             <div className="Endorsements" id="endorsements">
                 <div className="Endorsements__intro">
                     <p className="font_themed">Find out what businesses I've worked with have to say about me...</p>
                     <img src={quotes} alt="quotes"/>
                 </div>
                 <div className="Endorsements__quotes">
-                    <div className="quote">
-                        <p className="quote__business"><b>ted</b></p>
-                        <p className="quote__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                    </div>
-                    <div className="quote">
-                        <p className="quote__business"><b>National Museum of Scotland</b></p>
-                        <p className="quote__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                    <div className="quote">
-                        <p className="quote__business"><b>iab</b></p>
-                        <p className="quote__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
+                    {endorsementsList}
                 </div>
             </div>
         </section>
     )
+}
+function displayEndorsements(endorsement){
+    const {title, name, text, link} = endorsement;
+    return(
+        <div className="quote" key={name}>
+            <p className="quote__text">{text}</p>
+            <div className="quote__name">
+                <b>{name}</b> <br/>
+                <i><a href={link} target="_blank"  rel="noopener noreferrer"> {title}</a></i>
+            </div>
+        </div>
+    )
+
 }
